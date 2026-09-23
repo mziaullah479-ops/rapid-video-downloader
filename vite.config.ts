effc47b82e61405a51934f6566655fec66c87fb7
mziaullah479-ops <mziaullah479@gmail.com>
2026-09-23 07:45:07 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,15 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: 'assets/[name]-[hash]-v2.js',
+          chunkFileNames: 'assets/[name]-[hash]-v2.js',
+          assetFileNames: 'assets/[name]-[hash]-v2[extname]',
+        },
+      },
+    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
